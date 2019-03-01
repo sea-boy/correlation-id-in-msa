b69e75ead515445a9f13ce772953946731ef7246
jphong <redwinemark@gmail.com>
2019-03-01 22:48:49 +0900

    Apply http logging filter to services

--- backend-applications/a-service/src/main/java/com/header/correlation/config/ApplicationConfig.java
+++ backend-applications/a-service/src/main/java/com/header/correlation/config/ApplicationConfig.java
@@ -1,11 +1,13 @@
 package com.header.correlation.config;
 
 
+import com.header.http.config.EnableHttpLogging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCorrelationId
+@EnableHttpLogging
 @Configuration
 public class ApplicationConfig {
 
--- backend-applications/b-service/src/main/java/com/header/correlation/config/ApplicationConfig.java
+++ backend-applications/b-service/src/main/java/com/header/correlation/config/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package com.header.correlation.config;
 
+import com.header.http.config.EnableHttpLogging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCorrelationId
+@EnableHttpLogging
 @Configuration
 public class ApplicationConfig {
 
